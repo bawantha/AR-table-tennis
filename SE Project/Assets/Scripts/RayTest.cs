ebac325a4f939f68670ca76e3bcaade56d3810d8
bawantha <bawwa.16@cse.mrt.ac.lk>
2019-03-03 13:29:12 +0530

    touch object detec is done

---
+++ SE Project/Assets/Scripts/RayTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour {
+
+    public Camera fpsCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = fpsCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+
+                Debug.Log(hitInfo.transform.name);
+            }
+        }
+
+    }
+
+}
+
+    
