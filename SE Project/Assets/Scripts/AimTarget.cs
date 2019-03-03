bb5c4e0ca34e84cb8681102e29aeb65901d85d50
bawantha <bawwa.16@cse.mrt.ac.lk>
2019-03-04 01:48:42 +0530

    ball is hittinng to aim target directon
    net ekak dala na
    net ela athulen yanawa

---
+++ SE Project/Assets/Scripts/AimTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(-0.6f, 0.6f), 0.4990242f, -0.871f);
+
+    }
+
+}
---
+++ SE Project/Assets/Scripts/Racket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+    public GameObject aim,ball;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if(v != 0 || h!= 0)
+        {
+            transform.Translate(new Vector3(0, h, -v)*Time.deltaTime);
+        }
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Vector3 dir = aim.transform.position - ball.transform.position;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized*4f;
+        }
+        
+    }
+}
