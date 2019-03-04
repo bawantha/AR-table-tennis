3aa73b6125ec357596a19a90411a1e89fbf77d25
bawantha <bawwa.16@cse.mrt.ac.lk>
2019-03-04 13:06:41 +0530

    Game is done
    location of ball is pre defined location using Random numbers

--- SE Project/Assets/Scripts/Bot.cs
+++ SE Project/Assets/Scripts/Bot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bot : MonoBehaviour {
-    public Transform aim, ball;
+    public Transform aim, ball, aimTarget;
 
     Vector3 targetPotiton;
     // Use this for initialization
@@ -27,8 +27,9 @@ public class Bot : MonoBehaviour {
     {
         if (other.CompareTag("Ball"))
         {
+            aimTarget.transform.position = new Vector3(Random.Range(-0.6f, 0.6f), 0.4990242f, -0.871f);
             Vector3 dir = aim.transform.position - ball.transform.position;
-            other.GetComponent<Rigidbody>().velocity = dir.normalized * 3.5f + new Vector3(0, 2.5f, 0);
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * 3.5f + new Vector3(0, 2.7f, 0);
         }
 
     }
