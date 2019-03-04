656a36f6978c8df5899a216a4d7384bd1b17991b
bawantha <bawwa.16@cse.mrt.ac.lk>
2019-03-04 15:58:22 +0530

    Sound is added

--- SE Project/Assets/Scripts/Bot.cs
+++ SE Project/Assets/Scripts/Bot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bot : MonoBehaviour {
     public Transform aim, ball, aimTarget;
+    public AudioSource raackethit;
 
     Vector3 targetPotiton;
     // Use this for initialization
@@ -27,6 +28,7 @@ public class Bot : MonoBehaviour {
     {
         if (other.CompareTag("Ball"))
         {
+            raackethit.Play();
             aimTarget.transform.position = new Vector3(Random.Range(-0.6f, 0.6f), 0.4990242f, -0.871f);
             Vector3 dir = aim.transform.position - ball.transform.position;
             other.GetComponent<Rigidbody>().velocity = dir.normalized * 3.5f + new Vector3(0, 2.7f, 0);
